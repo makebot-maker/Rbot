0e5e30536d61f838b6571d05a9981f484954c5ed
WONDAEHAN <62107740+makebot-maker@users.noreply.github.com>
2020-11-18 21:43:01 +0900

    Add files via upload

---
+++ code/happysound.ino
@@ -0,0 +1,18 @@
+/* makebot R BOT happysound code
+ * makebot homepage(English) : http://makebot.mystrikingly.com/
+ * makebot homepage(Korean) : https://makebot.modoo.at
+ * files : https://github.com/makebot-lab/rbot
+ * R BOT is humanoid robot made by WONDAEHAN.
+ */
+#include <Rbot.h>
+
+Rbot Rbot;
+
+void setup() {
+  Rbot.ready();
+  Rbot.sound(1);
+}
+
+void loop() {
+  
+}
---
+++ code/hello.ino
@@ -0,0 +1,18 @@
+/* makebot R BOT hello code
+ * makebot homepage(English) : http://makebot.mystrikingly.com/
+ * makebot homepage(Korean) : https://makebot.modoo.at
+ * files : https://github.com/makebot-lab/rbot
+ * R BOT is humanoid robot made by WONDAEHAN.
+ */
+#include <Rbot.h>
+
+Rbot Rbot;
+
+void setup() {
+  Rbot.ready();
+  Rbot.motion(1);
+}
+
+void loop() {
+  
+}
